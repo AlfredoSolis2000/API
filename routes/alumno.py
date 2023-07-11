f883fb690dbb6f159d0c4264c02af54d853baa32
AlfredoSolis2000 <alfredo.solissanchez@utxicotepec.edu.mx>
2023-07-11 11:01:56 -0600

    first commit

---
+++ routes/alumno.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from config.db import conn
+from models.alumno import alumnos
+from schemas.alumno import Alumno
+router=APIRouter()
+
+@router.get('/getAll')
+def ObtenerAlumnos():
+    result=conn.execute(alumnos.select()).fetchall()
+    response=[]
+    for tuple in result:
+        alumno={
+            "matricula":tuple[0],
+            "nombre":tuple[1],
+            "apellidos":tuple[2],
+            "cuatrimestre":tuple[3],
+            "promedio":tuple[4],
+        }
+        response.append(alumno)
+    return response
+@router.post('/insert')
+def InsertarAlumno(alumno:Alumno):
+    conn.execute(alumnos.insert().values(dict(alumno)))
+    conn.commit()
+    res={
+        "status":"Alumno insertado con exito"
+    }
+    return res
+
+
